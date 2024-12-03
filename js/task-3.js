05d554971dcaed7f42db822f699e9ea6dca6b647
ArtCan <129169826+Dealwithgod@users.noreply.github.com>
2024-12-02 19:47:47 -0800

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,30 @@
+function checkForSpam(message) {
+    const normalizedMessage = message.toLowerCase();
+    if (normalizedMessage.includes('spam') || normalizedMessage.includes('sale')) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
+    }
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
 
-function getElementWidth(content, padding, border) {
-  // let result = Number.parseFloat(content) + Number.parseFloat(padding) + Number.parseFloat(border);
-  return Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-  // result;
-}
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+// Залиш цей код для перевірки ментором.
 
+// На що буде звертати увагу ментор при перевірці:
+
+// Оголошена функція checkForSpam(message).
+// Виклик функції checkForSpam("Latest technology news") повертає false
+// Виклик функції checkForSpam("JavaScript weekly newsletter")повертає false
+// Виклик функції checkForSpam("Get best sale offers now!") повертає true
+// Виклик функції checkForSpam("Amazing SalE, only tonight!") повертає true
+// Виклик функції checkForSpam("Trust me, this is not a spam message") повертає true
+// Виклик функції checkForSpam("Get rid of sPaM emails. Our book in on sale!") повертає true
+// Виклик функції checkForSpam("[SPAM] How to earn fast money?") повертає true
